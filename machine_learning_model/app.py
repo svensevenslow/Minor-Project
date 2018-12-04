a388819f079b991f58ed791f93ee0bd9e42fb3ad
Kainaat Singh <kainaat.singh@gmail.com>
2018-12-04 15:39:49 +0530

    Improvements in front end

--- machine_learning_model/app.py
+++ machine_learning_model/app.py
@@ -8,17 +8,13 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
  
-from flask import Flask, request
+from flask import Flask, request, jsonify
  
 @main.route("/<int:user_id>/ratings/top/<int:count>", methods=["GET"])
 def top_ratings(user_id, count):
     logger.debug("User %s TOP ratings requested", user_id)
     top_ratings = recommendation_engine.get_top_ratings(user_id,count)  
-    logger.info(top_ratings)
-    
-    with open ("output.txt","w") as fo:
-        fo.write(json.dumps(top_ratings))
-
+    logger.info(top_ratings) 
     return json.dumps(top_ratings) 
 @main.route("/", methods=["GET"])
 def hello():
@@ -31,7 +27,6 @@ def movie_ratings(user_id, movie_id):
     ratings = recommendation_engine.get_ratings_for_movie_ids(user_id, [movie_id])
     return json.dumps(ratings)
  
- 
 @main.route("/<int:user_id>/ratings", methods = ["POST"])
 def add_ratings(user_id):
     # get the ratings from the Flask POST request object
@@ -41,9 +36,7 @@ def add_ratings(user_id):
     ratings = map(lambda x: (user_id, int(x[0]), float(x[1])), ratings_list)
     # add them to the model using then engine API
     recommendation_engine.add_ratings(ratings)
- 
     return json.dumps(ratings)
- 
  
 def create_app(spark_context, dataset_path):
     global recommendation_engine 
--- website_database_server_connection/app.py
+++ website_database_server_connection/app.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
@@ -8,30 +9,73 @@ engine = create_engine('sqlite:///website_users.db', echo=True)
  
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' 
+sessions = []
+@app.route('/rating', methods=['GET', 'POST'])
+def rating():
+    filename = "/home/kainaat/Documents/PEC/Sem5/Minor_Project_on_github_repo/Minor-Project/ml-latest-small/moviesnew.csv"
+    x = pd.read_csv(filename) 
+    return render_template('star.html', name=filename, data=x.to_html())  
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    #return render_template('main2.html')
+    return render_template('homepage.html')  
+
+@app.route('/for_login', methods=['GET', 'POST'])
+def home_page():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        user_id_var = 1
-        Params = {'user_id' : user_id_var}
-        reco = requests.get('http://0.0.0.0:5001/', params = Params)
-        data = reco.json()
-        list_of_movies = data['recommend']
-        return render_template('recommendation.html', movie_list = list_of_movies)
+        return render_template('cinephile.html')
 
-@app.route('/signup', methods=['POST'])
+@app.route('/redirect_for_signup', methods=['GET'])
+def redirect_user():
+    return render_template('signup.html')
+
+@app.route('/recommendations', methods=['GET'])
+def recommend():
+    user_name = session['user_name']
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    user_name_query = s.query(User).filter(User.username.in_([user_name]))
+    result = user_name_query.first()
+    user_id_var = result.id
+    url = 'http://127.0.0.1:9000/'
+    url = url + str(user_id_var) + '/ratings/top/10'
+    reco = requests.get(url)
+    data = reco.json()
+    list_of_movies = data
+    return render_template('recommendation.html', movie_list = list_of_movies)
+
+@app.route('/redirect_for_signup', methods=['POST'])
 def register_user():
+    #print("POST called")
+    #return "hello from post"
+    
     POST_USERNAME = str(request.form['username'])
     POST_PASSWORD = str(request.form['password'])
+    
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]))
+    result = query.first()
+    if result:
+        flash('User already exists')
+        return home()
+    else:
+        user = User(POST_USERNAME, POST_PASSWORD)
+        s.add(user)
+        s.commit()
+        return home() 
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
  
     POST_USERNAME = str(request.form['username'])
     POST_PASSWORD = str(request.form['password'])
- 
+
+    session['user_name'] = POST_USERNAME
     Session = sessionmaker(bind=engine)
     s = Session()
     query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
@@ -40,12 +84,12 @@ def do_admin_login():
         session['logged_in'] = True
     else:
         flash('wrong password!')
-    return home()
- 
+    return home_page()
+
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    return home()
+    return home_page()
  
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
